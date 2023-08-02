bdb3db0cafc344247beb4de8323e0c3c67b54d24
vladislav-slobodenjuk <vladjun@gmail.com>
2023-08-02 23:25:47 +0300

    clean app comments

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,43 +1,11 @@
-// import { useEffect, useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
+// import { useEffect } from 'react';
 
 import { Container } from './App.styled';
 import { ContactForm } from '../ContactForm/ContactForm';
 import { ContactList } from '../ContactList/ContactList';
 import { Filter } from '../Filter/Filter';
 
-// import { getContacts, getFilter } from 'redux/selectors';
-// import { addContact, deleteContact } from 'redux/contactsSlice';
-// import { setFilterContacts } from 'redux/filterSlice';
-
 export const App = () => {
-  // const { contacts } = useSelector(getContacts);
-  // const filter = useSelector(getFilter);
-
-  // const dispatch = useDispatch();
-
-  // const handleFilterChange = ({ target: { value } }) => {
-  // dispatch(setFilterContacts(value));
-  // };
-
-  // const handleAddContact = ({ name, number }) => {
-  //   const isExist = contacts.find(contact => contact.name === name);
-  //   if (isExist) return alert(`${name} is already in contacts.`);
-
-  //   dispatch(addContact({ name, number }));
-  // };
-
-  // const handleDeleteContact = id => {
-  //   dispatch(deleteContact(id));
-  // };
-
-  // const filterContacts = () => {
-  //   const normalizedFilter = filter.toLowerCase();
-  //   return contacts.filter(({ name }) =>
-  //     name.toLowerCase().includes(normalizedFilter)
-  //   );
-  // };
-
   // useEffect(() => {
   //   const savedContacts = JSON.parse(localStorage.getItem(STORAGE_KEY));
   //   // console.log('savedContacts', savedContacts);
@@ -48,8 +16,6 @@ export const App = () => {
   //   localStorage.setItem(STORAGE_KEY, JSON.stringify(contacts));
   //   // console.log('second');
   // }, [contacts]);
-
-  // const filteredContacts = filterContacts();
 
   return (
     <Container>
